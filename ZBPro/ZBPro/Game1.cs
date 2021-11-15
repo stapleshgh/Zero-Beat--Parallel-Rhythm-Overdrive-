572fcbddc3932c7c0ae152b16db540d302aa68bd
stapleshgh <92657664+stapleshgh@users.noreply.github.com>
2021-11-15 14:25:02 -0500

    project files
    literally the bare minimum

---
+++ ZBPro/ZBPro/Game1.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace ZBPro
+{
+    public class Game1 : Game
+    {
+        Texture2D texture;
+        private GraphicsDeviceManager _graphics;
+        SpriteBatch spriteBatch;
+        Vector2 position;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+            position = new Vector2(0, 0);
+        }
+
+        protected override void Initialize()
+        {
+            texture = new Texture2D(this.GraphicsDevice, 100, 100);
+            Color[] colorData = new Color[100 * 100];
+            for (int i = 0; i < 10000; i ++)
+            {
+                colorData[i] = Color.White;
+            }
+
+            texture.SetData<Color>(colorData);
+
+
+            base.Initialize();
+        }
+
+        protected override void OnActivated(object sender, EventArgs args)
+        {
+            this.Window.Title = "Active Application";
+            base.OnActivated(sender, args);
+        }
+
+        protected override void OnDeactivated(object sender, EventArgs args)
+        {
+            this.Window.Title = "Inactive Application";
+            base.OnDeactivated(sender, args);
+        }
+
+        protected override void LoadContent()
+        {
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            texture = Content.Load<Texture2D>("background");
+
+            
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (IsActive)
+            {
+                if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                    Exit();
+            }
+            position.X += 200.0f * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (position.X > this.GraphicsDevice.Viewport.Width)
+                position.X = 0;
+
+            base.Update(gameTime);
+            
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture, position, Color.White);
+            spriteBatch.End();
+
+            
+
+            base.Draw(gameTime);
+        }
+    }
+}
