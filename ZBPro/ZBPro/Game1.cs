beaa66380a9e17ae91bd1af0db127815eab4740c
stapleshgh <92657664+stapleshgh@users.noreply.github.com>
2021-11-15 23:49:36 -0500

    more stuff

--- ZBPro/ZBPro/Game1.cs
+++ ZBPro/ZBPro/Game1.cs
@@ -8,19 +8,23 @@ namespace ZBPro
     public class Game1 : Game
     {
         Texture2D texture;
-        private GraphicsDeviceManager _graphics;
+        private GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Vector2 position;
 
+
+        //Constructor, mostly used to make valid objects for use later in the program
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             Window.AllowUserResizing = true;
             position = new Vector2(0, 0);
         }
 
+
+        //Long-running initializations that typically last longer
         protected override void Initialize()
         {
             texture = new Texture2D(this.GraphicsDevice, 100, 100);
@@ -36,18 +40,24 @@ namespace ZBPro
             base.Initialize();
         }
 
+
+        //Defines what to do while the window is in focus
         protected override void OnActivated(object sender, EventArgs args)
         {
             this.Window.Title = "Active Application";
             base.OnActivated(sender, args);
         }
 
+
+        //Defines what to do if the window is clicked off of
         protected override void OnDeactivated(object sender, EventArgs args)
         {
             this.Window.Title = "Inactive Application";
             base.OnDeactivated(sender, args);
         }
 
+
+        //Loads all game content, like assets and music and such
         protected override void LoadContent()
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
@@ -57,6 +67,8 @@ namespace ZBPro
             
         }
 
+
+        //Runs at a speed defined by gameTime, which is the time since the last frame call
         protected override void Update(GameTime gameTime)
         {
             if (IsActive)
@@ -73,6 +85,8 @@ namespace ZBPro
             
         }
 
+
+        //Updates the screen and the objects on the screen
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
